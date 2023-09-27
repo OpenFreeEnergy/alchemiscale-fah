7264fbf3e072699f849d62e33958cc1733086349
David Dotson <dotsdl@gmail.com>
2023-09-26 20:32:28 -0700

    Adding more skeletal bits

---
+++ alchemiscale_fah/compute/service.py
@@ -0,0 +1,9 @@
+"""
+:mod:`alchemiscale_fah.compute.service` --- compute services for FEC execution via Folding@Home
+===============================================================================================
+
+"""
+
+
+class FAHComputeService:
+    ...
